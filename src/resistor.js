ab742eddc94025b25848b4df34125eec5ca328e1
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-10-09 10:41:16 -0400

    Complete resistor.js tests

--- src/resistor.js
+++ src/resistor.js
@@ -62,7 +62,19 @@
  * then use the copied object like a lookup table
  */
 function getColorValue(color) {
-  // write your code here & return value
+  const colorCodes = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
+  return colorCodes[color];
 }
 
 /**
@@ -79,7 +91,21 @@ function getColorValue(color) {
  * then use the copied object like a lookup table
  */
 function getMultiplierValue(color) {
-  // write your code here & return value
+  const multiplierCodes = {
+    black: 1,
+    brown: 10,
+    red: 100,
+    orange: 1000,
+    yellow: 10000,
+    green: 100000,
+    blue: 1000000,
+    violet: 10000000,
+    grey: 100000000,
+    white: 1000000000,
+    gold: 0.1,
+    silver: 0.01,
+  };
+  return multiplierCodes[color];
 }
 
 /**
@@ -106,7 +132,15 @@ function getMultiplierValue(color) {
  *
  */
 function getThreeBandValue(bands) {
-  // write your code here & return value
+  const firstTwo = (getColorValue(bands.color1) * 10) + getColorValue(bands.color2);
+  const finalValue = firstTwo * getMultiplierValue(bands.multiplier);
+  if (bands.multiplier === 'gold') {
+    return parseFloat(finalValue.toFixed(1));
+  }
+  if (bands.multiplier === 'silver') {
+    return parseFloat(finalValue.toFixed(2));
+  }
+  return finalValue;
 }
 
 /**
@@ -131,7 +165,16 @@ function getThreeBandValue(bands) {
  *
  */
 function formatNumber(val) {
-  // write your code here & return value
+  if (val >= 1000 && val <= 1000000) {
+    return `${val / 1000}k`;
+  }
+  if (val >= 1000000 && val <= 1000000000) {
+    return `${val / 1000000}M`;
+  }
+  if (val >= 1000000000) {
+    return `${val / 1000000000}G`;
+  }
+  return val.toString();
 }
 
 /**
@@ -150,7 +193,17 @@ function formatNumber(val) {
  * example: 'green' => '±0.5%'
  */
 function getTolerance(color) {
-  // write your code here & return value
+  const toleranceCodes = {
+    brown: '±1%',
+    red: '±2%',
+    green: '±0.5%',
+    blue: '±0.25%',
+    violet: '±0.1%',
+    grey: '±0.05%',
+    gold: '±5%',
+    silver: '±10%',
+  };
+  return toleranceCodes[color];
 }
 
 /**
@@ -182,7 +235,7 @@ function getTolerance(color) {
  * must use functions in this file to build the string using a template literal
  */
 function getResistorOhms(bands) {
-  // write your code here & return value
+  return `Resistor value: ${formatNumber(getThreeBandValue(bands))} Ohms ${getTolerance(bands.tolerance)}`;
 }
 
 module.exports = {
