b36af05157e3890ada6b04bfb0a074757495f44e
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-10-09 09:42:11 -0400

    Complete objects-01.js tests

--- src/objects-01.js
+++ src/objects-01.js
@@ -27,7 +27,15 @@
  *
  */
 function makePetObject() {
-  // write your code here & return value
+  const pet = {
+    name: '',
+    age: 0,
+    species: '',
+    hasFur: true,
+    legs: 0,
+    bestFriend: '',
+  };
+  return pet;
 }
 
 /**
@@ -39,7 +47,7 @@ function makePetObject() {
  * Mozilla - https://mzl.la/3iu0uh4
  */
 function getPetName(pet) {
-  // write your code here & return value
+  return pet.name;
 }
 
 /**
@@ -52,7 +60,7 @@ function getPetName(pet) {
  */
 function getPetAge(pet) {
   // eslint-disable-next-line dot-notation
-  // write your code here & return value
+  return pet.age;
 }
 
 /**
@@ -69,7 +77,7 @@ function getPetAge(pet) {
  * permanently making a change to the passed object.
  */
 function setPetAge(pet, age) {
-  // write your code here & return value
+  pet.age = age;
 }
 
 /**
@@ -78,7 +86,7 @@ function setPetAge(pet, age) {
  * @returns {boolean} - true if the pet's age is < 1 , false otherwise
  */
 function isPetABaby(pet) {
-  // write your code here & return value
+  return (pet.age < 1);
 }
 
 /**
@@ -89,7 +97,7 @@ function isPetABaby(pet) {
  * breed must be the property name that is added
  */
 function addPetBreed(pet, str) {
-  // write your code here & return value
+  pet.breed = str;
 }
 
 /**
@@ -100,7 +108,8 @@ function addPetBreed(pet, str) {
  * Mozilla - https://mzl.la/3FmDpqv
  */
 function deletePetBestFriend(pet) {
-  // write your code here & return value
+  delete pet.bestFriend;
+  return pet;
 }
 
 /**
@@ -111,7 +120,7 @@ function deletePetBestFriend(pet) {
  * Mozilla - https://mzl.la/3ipqBW8
  */
 function getPetKeys(pet) {
-  // write your code here & return value
+  return Object.keys(pet);
 }
 
 /**
@@ -121,7 +130,7 @@ function getPetKeys(pet) {
  * book - https://bit.ly/3uz2fOy
  */
 function getPetObjLength(pet) {
-  // write your code here & return value
+  return Object.keys(pet).length;
 }
 
 module.exports = {
