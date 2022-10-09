7378819c7d193d82709ab6c28c2f2e28f0d3283d
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-10-09 09:57:06 -0400

    Complete objects-03.js test

--- src/objects-03.js
+++ src/objects-03.js
@@ -24,7 +24,11 @@
  * example: getVolumeOfBox({length: 2.4, width: 3, height: 1.8}) // => 12.96
  */
 function getVolumeOfBox(box) {
-  // write your code here & return value
+  const volume = (box.length * box.width * box.height);
+  if (volume % 1 === 0) {
+    return volume;
+  }
+  return parseFloat(volume.toFixed(2));
 }
 
 module.exports = {
